dad704b0c3335ec08f0f1abc4e46b24b0e238c5b
Jackarain <jack.wgm@gmail.com>
2019-09-28 10:07:56 +0800

    add test

--- odbtest/main.cpp
+++ odbtest/main.cpp
@@ -11,16 +11,30 @@
 #include <odb/pgsql/database.hxx>
 #include <odb/schema-catalog.hxx>
 
+#include <soci/soci.h>
+#include <soci/postgresql/soci-postgresql.h>
+
+using namespace soci;
+using namespace std;
+
+
 int main()
 {
 	try
 	{
+		session sql(postgresql, "user='postgres' dbname='ftest' host='192.168.2.110' port=5432 application_name=odb-release");
+		int count;
+		sql << "select count(*) from spool_sealed_share", into(count);
+		auto bk = sql.get_backend();
+
 		std::unique_ptr<odb::pgsql::connection_factory>
 			pool(new odb::pgsql::connection_pool_factory(100, 50));
 
 		auto db = std::make_shared<odb::pgsql::database>("postgres",
-			"postgres", "postgres", "127.0.0.1", 5432,
+			"", "postgres", "192.168.2.110", 5432,
 			"application_name=odb-release", std::move(pool));
+
+		auto pghandle = db->connection()->handle();
 
 		odb::transaction t(db->begin());
 		if (db->schema_version() == 0)
