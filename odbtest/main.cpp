c4a4dfb7f490ba367f10477963712e4395acfebd
Jackarain <jack.wgm@gmail.com>
2019-09-29 10:48:33 +0800

    Update example

--- odbtest/main.cpp
+++ odbtest/main.cpp
@@ -22,35 +22,71 @@ int main()
 {
 	try
 	{
-		session sql(postgresql, "user='postgres' dbname='ftest' host='192.168.2.110' port=5432 application_name=odb-release");
-		int count;
-		sql << "select count(*) from spool_sealed_share", into(count);
-		auto bk = sql.get_backend();
-
 		std::unique_ptr<odb::pgsql::connection_factory>
 			pool(new odb::pgsql::connection_pool_factory(100, 50));
 
 		auto db = std::make_shared<odb::pgsql::database>("postgres",
-			"", "postgres", "192.168.2.110", 5432,
+			"", "ftest", "192.168.2.110", 5432,
 			"application_name=odb-release", std::move(pool));
 
-		auto pghandle = db->connection()->handle();
+		{
+			odb::transaction t(db->begin());
+			if (db->schema_version() == 0)
+			{
+				t.commit();
+				t.reset(db->begin());
+				odb::schema_catalog::create_schema(*db, "", true);
+				t.commit();
+			}
+			else
+			{
+				t.commit();
+				t.reset(db->begin());
+				odb::schema_catalog::migrate(*db);
+				t.commit();
+			}
+		}
 
-		odb::transaction t(db->begin());
-		if (db->schema_version() == 0)
 		{
-			t.commit();
-			t.reset(db->begin());
-			odb::schema_catalog::create_schema(*db, "", true);
+			auto dbconn = db->connection();
+			odb::transaction t(dbconn->begin());
+
+			auto pghandle = dbconn->handle();
+
+			session sql((void*)pghandle);
+
+			int count;
+			sql << "select count(*) from spool_sealed_share", into(count);
+
+			int id;
+			std::string miner;
+			std::string nonce = "0xd9d64d7dfb04ecf0";
+			sql << "select id, miner from spool_sealed_share where nonce = :nonce",
+				into(id), into(miner), use(nonce);
+
 			t.commit();
 		}
-		else
+
 		{
-			t.commit();
-			t.reset(db->begin());
-			odb::schema_catalog::migrate(*db);
+			auto dbconn = db->connection();
+			odb::transaction t(dbconn->begin());
+
+			auto pghandle = dbconn->handle();
+
+			session sql((void*)pghandle);
+
+			int count;
+			sql << "select count(*) from spool_sealed_share", into(count);
+
+			int id;
+			std::string miner;
+			std::string nonce = "0xd9d64d7dfb04ecf0";
+			sql << "select id, miner from spool_sealed_share where nonce = :nonce",
+				into(id), into(miner), use(nonce);
+
 			t.commit();
 		}
+
 	}
 	catch (const std::exception& e)
 	{
