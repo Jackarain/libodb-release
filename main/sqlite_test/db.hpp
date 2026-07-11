6e954fb9f1376d114273c457384c691fa79f6109
Jackarain <jack.wgm@gmail.com>
2026-07-11 13:23:46 +0800

    添加 trraits-sqlite.hpp 用于将来映射 sqlite 类型到 c++ 类型

--- main/sqlite_test/db.hpp
+++ main/sqlite_test/db.hpp
@@ -1,9 +1,26 @@
 #pragma once
 
+#include <vector>
+#include <sstream>
+#include <iostream>
+#include <cstring> // std::memcpy
+
+#include <odb/section.hxx>
+#include <odb/nullable.hxx>
+
+#include <boost/date_time/posix_time/ptime.hpp>
+
+#ifdef _MSC_VER
+#	pragma warning (push)
+#	pragma warning (disable:4068)
+#endif // _MSC_VER
+
+#pragma db model version(1, 1, open)
+
 #pragma db object
 struct odbtest_config {
-    #pragma db id
-    unsigned long id;
+#pragma db id
+    std::string id_;
 
-    int value;
+    std::string params;
 };
---
+++ main/sqlite_test/traits-sqlite.hpp
@@ -0,0 +1,23 @@
+﻿//
+// Copyright (C) 2019 Jack.
+//
+// Author: jack
+// Email:  jack.wgm at gmail dot com
+//
+
+#pragma once
+
+#include <cstddef>	// std::size_t
+#include <cstring>	// std::strncmp, std::memset, std::memcpy
+#include <sstream>	// std::istringstream
+#include <vector>	// std::vector
+#include <string>	// std::string
+
+#include <odb/sqlite/traits.hxx>
+
+#include "db.hpp"
+
+namespace odb {
+	namespace sqlite {
+	}
+}
