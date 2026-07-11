e96976f414a2aa1bff1f21cd88829cc54045a95a
Jackarain <jack.wgm@gmail.com>
2026-07-11 13:06:01 +0800

    修正 cmake 处理 .ixx 文件时在 msvc 中错误的当作 c++ 模块文件的问题

--- cmake/odb.cmake
+++ cmake/odb.cmake
@@ -34,36 +34,36 @@ else()
 endif()
 
 add_custom_command(OUTPUT ${OUT_DIR}/${ODB_HEADER_BASE}.sql
-                   COMMAND ${ODB_COMPILER} ARGS
-                              ${EXTRA_ARGS}
-                              -d ${ODB_DB_TYPE} --std c++11 -p boost --generate-schema-only --schema-format sql ${ODB_DB_OPTIONS} --hxx-prologue "#include \"db_traits.hpp\""
-                              ${ODB_HEADER}
-                              ${ODB_INC_DIRS}
-                   MAIN_DEPENDENCY ${ODB_HEADER}
-                   DEPENDS ${ODB_HEADER}
-                   WORKING_DIRECTORY ${OUT_DIR}
-                   COMMENT "generating SQL definations for ${ODB_HEADER_BASE}"
-                   VERBATIM)
+					COMMAND ${ODB_COMPILER} ARGS
+					${EXTRA_ARGS}
+					-d ${ODB_DB_TYPE} --std c++11 -p boost --generate-schema-only --schema-format sql ${ODB_DB_OPTIONS} --hxx-prologue "#include \"db_traits.hpp\""
+					${ODB_HEADER}
+					${ODB_INC_DIRS}
+					MAIN_DEPENDENCY ${ODB_HEADER}
+					DEPENDS ${ODB_HEADER}
+					WORKING_DIRECTORY ${OUT_DIR}
+					COMMENT "generating SQL definations for ${ODB_HEADER_BASE}"
+					VERBATIM)
 
 add_custom_command(OUTPUT
-                          ${OUT_DIR}/${ODB_HEADER_BASE}-odb.cxx
-                          ${OUT_DIR}/${ODB_HEADER_BASE}-odb.hxx
-                          ${OUT_DIR}/${ODB_HEADER_BASE}-odb.ixx
-                   COMMAND ${ODB_COMPILER} ARGS ${EXTRA_ARGS}
-                              -d ${ODB_DB_TYPE} --std c++11 -p boost --generate-query --generate-schema --schema-format embedded ${ODB_DB_OPTIONS} --hxx-prologue "#include \"db_traits.hpp\""
-                              ${ODB_HEADER}
-                              ${ODB_INC_DIRS}
-                   MAIN_DEPENDENCY ${ODB_HEADER}
-                   DEPENDS ${ODB_HEADER}
-                   WORKING_DIRECTORY ${OUT_DIR}
-                   COMMENT "generating ODB bindings for ${ODB_HEADER_BASE}"
-                   VERBATIM)
-
-set(${ODB_HEADERS})
+					${OUT_DIR}/${ODB_HEADER_BASE}-odb.cxx
+					${OUT_DIR}/${ODB_HEADER_BASE}-odb.hxx
+					${OUT_DIR}/${ODB_HEADER_BASE}-odb.ixx
+					COMMAND ${ODB_COMPILER} ARGS ${EXTRA_ARGS}
+					-d ${ODB_DB_TYPE} --std c++11 -p boost --generate-query --generate-schema --schema-format embedded ${ODB_DB_OPTIONS} --hxx-prologue "#include \"db_traits.hpp\""
+					${ODB_HEADER}
+					${ODB_INC_DIRS}
+					MAIN_DEPENDENCY ${ODB_HEADER}
+					DEPENDS ${ODB_HEADER}
+					WORKING_DIRECTORY ${OUT_DIR}
+					COMMENT "generating ODB bindings for ${ODB_HEADER_BASE}"
+					VERBATIM)
 
 set(${ODB_HEADERS}
 	${OUT_DIR}/${ODB_HEADER_BASE}-odb.cxx
 	${OUT_DIR}/${ODB_HEADER_BASE}-odb.hxx
-	${OUT_DIR}/${ODB_HEADER_BASE}-odb.ixx PARENT_SCOPE)
+	#${OUT_DIR}/${ODB_HEADER_BASE}-odb.ixx # 不向外暴露该 .ixx 文件，原因是 msvc 会将 .ixx 当成模块文件处理而导致出错.
+	PARENT_SCOPE
+)
 
 endfunction(ADD_ODB_INTERFACE)
